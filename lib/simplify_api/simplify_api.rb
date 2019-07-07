94f2b047120d2dec1178c8db7031fb97f46746ab
Rodrigo Couto <r@rodg.co>
2019-07-07 04:08:09 +0000

    rspec + a lot of refactoring

--- lib/simplify_api/simplify_api.rb
+++ lib/simplify_api/simplify_api.rb
@@ -1,50 +1,55 @@
+# frozen_string_literal: true
+
+# SimplifyApi
 module SimplifyApi
   def self.included(base)
     base.singleton_class.send(:attr_accessor, :attributes)
     base.attributes = {}
-    base.singleton_class.send(:attr_accessor, :mandatory)
-    base.mandatory = []
-    base.singleton_class.send(:attr_accessor, :default)
-    base.default = {}
     base.extend(ClassMethods)
   end
 
+  # ClassMethods
   module ClassMethods
     def attribute(attr, type = Object, **args)
-      raise ArgumentError, "Duplicate attribute #{attr}." if self.attributes[attr]
-      if type.class == Array then
+      raise ArgumentError, "Duplicate attribute #{attr}." if attributes[attr]
+
+      if type.class == Array
         args[:default] = [] unless args[:default]
         args[:array_type] = type[0]
       end
-      self.attributes[attr] = {
+
+      args[:mandatory] ||= false
+      args[:default] ||= nil unless args[:default].class == FalseClass
+
+      attributes[attr] = {
         name: attr.to_s,
-        type: type.class == Class ? type : type.class == Array ? Array : Object,
+        type: type.class == Class ? type : (type.class == Array ? Array : Object),
         params: args
       }
-      self.mandatory << attr if args[:mandatory]
-      self.default["#{attr}".to_sym] = args[:default] if args[:default]
-      self.attributes[attr]
+      attributes[attr]
     end
   end
 
-  def initialize(opts)
-    opts = {self.class.attributes.first[0] => opts} if opts.class == Array
-    opts.each_pair do |k, v|
-      case v
-      when Hash
-        v = self.class.attributes[k.to_sym][:type].new(v)
-      when Array
-        v.collect! do |i|
-          self.class.attributes[k.to_sym][:params][:array_type].new(i)
-        end
+  def initialize(opts = {})
+    opts.symbolize_keys!
+    # opts = { klass_attr.first[0] => opts } if opts.class == Array
+    klass_attr.each_pair do |name, spec|
+      params = spec[:params]
+      if opts.key?(name)
+        value = process_value(name, opts[name])
+        opts.delete(name)
+      else
+        value = params[:default]
       end
-      create_and_set_instance_variable("#{k}", v)
+      raise ArgumentError, "Missing mandatory attribute => #{name}" if params[:mandatory] & value.nil?
+
+      create_and_set_instance_variable(name.to_s, value)
     end
-    raise ArgumentError, "Missing mandatory attributes => #{self.class.mandatory-self.instance_variables}" if not mandatory_attributes_in?
-    self.class.default.each_pair do |k, v|
-      create_and_set_instance_variable(k, v) unless self.instance_variables.include?("@#{k}".to_sym)
+
+    opts.each_pair do |key, value|
+      expanded_value = process_value(key, value)
+      create_and_set_instance_variable(key.to_s, expanded_value)
     end
-    self
   end
 
   def to_h
@@ -54,8 +59,8 @@ module SimplifyApi
       v = instance_variable_get(i)
       if v.class == Array then
         r = []
-        v.each {|a| r << (a.respond_to?(:to_h) ? a.to_h : a) }
-        if self.class.attributes[k][:params][:invisible] then
+        v.each { |a| r << (a.respond_to?(:to_h) ? a.to_h : a) }
+        if klass_attr[k][:params][:invisible] then
           h = r
         else
           h[k] = r
@@ -70,16 +75,28 @@ module SimplifyApi
   def to_json
     self.to_h.to_json
   end
-  
+
+  # method_missing
+  #   Called in case an unexisting method is called.
+  #
+  #   To an assignment call it will create the instance variable.
+  #   Every other call will be passed to super.
+  #
   def method_missing(method_name, *args, &block)
     case method_name
     when /(.*)\=$/
-      create_and_set_instance_variable("#{$1}", args[0])
+      create_and_set_instance_variable($1.to_s, args[0])
     else
       super(method_name, args, block)
     end
   end
 
+  # respond_to_mssing?
+  #   Called to check if an instance respond to a message.
+  #
+  #   It should respond to any assignment call.
+  #   Every other type should be passed to super.
+  #
   def respond_to_missing?(method_name, *args)
     case method_name
     when /(.*)\=$/
@@ -90,19 +107,49 @@ module SimplifyApi
   end
 
   private
+
   def create_and_set_instance_variable(name, value)
-    define_singleton_method("#{name}=") do |arg| 
-      valid_values = self.class.attributes[name.to_sym][:params][:values]
-      raise "Argument Error. Invalid value for #{name} => #{arg}" if valid_values && !valid_values.include?(arg)
+    klass_attr[name.to_sym] = { name: name, type: value.class, params: { default: nil, mandatory: false }} unless klass_attr[name.to_sym]
+
+    define_singleton_method("#{name}=") do |arg|
+      raise ArgumentError, "Invalid value for #{name} => #{arg}" unless valid_value?(name, arg)
+
       instance_variable_set("@#{name}", arg)
     end
-    define_singleton_method("#{name}") do
+
+    define_singleton_method(name.to_s) do
       instance_variable_get("@#{name}")
     end
-    self.send("#{name}=", value)
+
+    send("#{name}=", value)
   end
 
-  def mandatory_attributes_in?
-    self.class.mandatory.collect {|v| self.instance_variables.include?("@#{v}".to_sym)}.inject(&:&)
+  def valid_value?(name, value)
+    return true unless klass_attr[name.to_sym][:params].key?(:values)
+
+    valid_values = klass_attr[name.to_sym][:params][:values]
+    return true if valid_values.include?(value)
+
+    false
+  end
+
+  def process_value(key, value)
+    case value
+    when Hash
+      value = klass_attr[key][:type].new(value)
+    when Array
+      value.collect! do |item|
+        if klass_attr[key][:params].key?(:array_type)
+          klass_attr[key][:params][:array_type].new(item)
+        else
+          item
+        end
+      end
+    end
+    value
+  end
+
+  def klass_attr
+    self.class.attributes
   end
 end
